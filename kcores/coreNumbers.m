e826c2665785be098384feb6d52757488b033931
Rodrigo L.C <rodrigolealc@ciencias.unam.mx>
2017-02-21 14:19:50 +0000

    Forgotten file

--- kcores/coreNumbers.m
+++ kcores/coreNumbers.m
@@ -1,16 +1,16 @@
-function [bucket, core_nbs] = coreNumbers(country)
+function [bucket, core_nbs] = coreNumbers(edges, country)
 
 nn = country.num_nodes;
 
 % Initialise numbers d_v
-dvs = structDegree(country);
+[neighs, dvs] = neighbours(edges, country); % Second argument is degrees
 M = max(dvs);
 
 D = cell(M+1, 1);
 
-for i = 0:M
-    idx = dvs == i;
-    D{i+1} = country.V(idx);
+for deg = 0:M
+    idx = dvs == deg;
+    D{deg+1} = country.V(idx);
 end
 
 k = 0; % the degeneracy number
@@ -22,18 +22,18 @@ for j = 1:nn
         if ~isempty(D{i})
             actual_degree = i-1; % -1 because of indexing, to include D(0)
             k = max(k, actual_degree); 
-            core_nbs(end + 1 - j) = k;
+            core_nbs(end + 1 - j) = k; % the core number of the node we select below
             
             % Select a node
             v = D{i}(1);
-            bucket = [v; bucket];
-            D{i} = D{i}(2:end);
+            bucket = [v; bucket]; % add to bucket
+            D{i} = D{i}(2:end); % remove from D
             
-            neighs = neighbours(country, v);
+            ns = neighs{country.V == v};
             % The neighbours that are not already in bucket
-            neighs = neighs(~ismember(neighs, bucket));
+            ns = ns(~ismember(ns, bucket));
             
-            for u = neighs'
+            for u = ns'
                 lin_idx = country.V == u;
                 du = dvs(lin_idx);
                 dvs(lin_idx) = du - 1;
